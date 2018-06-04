b17a3a1b21f8f1ff457afceb18a871aa0ba0a888
vidus77 <vidus77@gmail.com>
2018-06-04 18:38:35 +0300

    first

--- VZSaveTheHumans/MainWindow.xaml.cs
+++ VZSaveTheHumans/MainWindow.xaml.cs
@@ -10,19 +10,19 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace VZSaveTheHumans
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for MAinPage.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MAinPage : Window
     {
-        public MainWindow()
+        public MAinPage()
         {
             InitializeComponent();
         }
+
     }
 }
--- VZSaveTheHumans/Properties/AssemblyInfo.cs
+++ VZSaveTheHumans/Properties/AssemblyInfo.cs
@@ -10,9 +10,9 @@ using System.Windows;
 [assembly: AssemblyTitle("VZSaveTheHumans")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("VZSaveTheHumans")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCompany("A-T")]
+[assembly: AssemblyProduct("SaveTheHumans tutorial Game")]
+[assembly: AssemblyCopyright("Copyright © Vida Zoltan 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,7 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: NeutralResourcesLanguage("ru-RU")]
+
